3c4ec7266f3069ff3dfddf8e43565c64c56ed2b2
sandro_herbst <sandro.herbst@gmail.com>
2019-11-13 09:24:53 -0300

    soma

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ app.get('/',(request, response) => {
     response.send(request.query)
 })
 
+app.get('/soma', (request, response) => {
+    const a = parseInt(request.query.a)
+    const b = parseInt(request.query.b)
+    const soma = a + b
+    response.send('<h1>A soma é: ' + soma + '</h1>') 
+})
+
 app.listen(3000)
