e40b645fb438d20fac0d6550ede146515fa9ad81
sandro_herbst <sandro.herbst@gmail.com>
2019-11-13 09:18:15 -0300

    query params

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(request, response) => {
-    response.send('Olá Fullstack Lab!')
+    response.send(request.query)
 })
 
 app.listen(3000)
