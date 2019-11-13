b1aa06f4413aee58ced2e923a9db4c27be54c236
sandro_herbst <sandro.herbst@gmail.com>
2019-11-13 09:56:32 -0300

    install body-parser

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require('body-parser')
 
 app.set('view engine','ejs')
 app.set('views', path.resolve(__dirname, 'views'))
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/',(request, response) => {
     response.render('home')
 })
 
 app.post('/',(request, response) => {
-    response.send('postou')
+    response.send(request.body)
 })
 
 app.get('/soma', (request, response) => {
