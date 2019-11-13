e3b84c9b2b04dfe7f9f652a1560f211d47895a89
sandro_herbst <sandro.herbst@gmail.com>
2019-11-13 10:59:26 -0300

    post bug to spreadsheet

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
 
+const GoogleSpreadsheet = require('google-spreadsheet')
+const credentials = require('./bugtracker.json')
+
+//config
+const docId = '1c3pwSVjWcoIbXq2thIvvDSVms0HD5v-HseZ90WKf44A'
+const worksheetIndex = 0
+
 app.set('view engine','ejs')
 app.set('views', path.resolve(__dirname, 'views'))
 
@@ -13,7 +20,23 @@ app.get('/',(request, response) => {
 })
 
 app.post('/',(request, response) => {
-    response.send(request.body)
+
+    const doc = new GoogleSpreadsheet(docId)
+
+    doc.useServiceAccountAuth(credentials, (err) => {
+        if(err) {
+            console.log('não foi possível abrir a planilha')
+        } else {
+            console.log('planilha aberta')
+            doc.getInfo((err, info) => {
+                const worksheet = info.worksheets[worksheetIndex]
+                worksheet.addRow({name: request.body.name, email: request.body.email}, err => {
+                    response.send('bug reportado com sucesso')
+                })
+            })
+        }
+    })
+
 })
 
 app.get('/soma', (request, response) => {
@@ -23,4 +46,10 @@ app.get('/soma', (request, response) => {
     response.send('<h1>A soma é: ' + soma + '</h1>') 
 })
 
-app.listen(3000)
+app.listen(3000, (err) => {
+    if (err) {
+        console.log('aconteceu um erro', err)
+    } else {
+        console.log('bugtracker rodando na porta 3000')
+    }
+})
