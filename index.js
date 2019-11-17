26f2b218b5e81d48d7bae1472f5ee51ae1e6163b
sandro_herbst <sandro.herbst@gmail.com>
2019-11-16 22:05:10 -0300

    SendGrid

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
+const { promisify } = require('util')
 
 const GoogleSpreadsheet = require('google-spreadsheet')
 const credentials = require('./bugtracker.json')
 
+const sgMail = require('@sendgrid/mail')
+
+
 //config
 const docId = '1c3pwSVjWcoIbXq2thIvvDSVms0HD5v-HseZ90WKf44A'
 const worksheetIndex = 0
+const sendGridKey = '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'
 
 app.set('view engine','ejs')
 app.set('views', path.resolve(__dirname, 'views'))
@@ -19,33 +24,49 @@ app.get('/',(request, response) => {
     response.render('home')
 })
 
-app.post('/',(request, response) => {
-
-    const doc = new GoogleSpreadsheet(docId)
-
-    doc.useServiceAccountAuth(credentials, (err) => {
-        if(err) {
-            console.log('não foi possível abrir a planilha')
-        } else {
-            console.log('planilha aberta')
-            doc.getInfo((err, info) => {
-                const worksheet = info.worksheets[worksheetIndex]
-                worksheet.
-                worksheet.addRow(
-                    { name: request.body.name
-                    , email: request.body.email
-                    , issueType: request.body.issueType
-                    , howToReproduce: request.body.howToReproduce
-                    , expectedOutput: request.body.expectedOutput
-                    , receivedOutput: request.body.receivedOutput
-                    }, err => {
-                    response.send('bug reportado com sucesso')
-                })
-            })
-        }
+app.post("/", async (request, response) => {
+  try {
+    const doc = new GoogleSpreadsheet(docId);
+    await promisify(doc.useServiceAccountAuth)(credentials);
+    console.log("planilha aberta");
+    const info = await promisify(doc.getInfo)();
+    const worksheet = info.worksheets[worksheetIndex];
+    await promisify(worksheet.addRow)({
+      name: request.body.name,
+      email: request.body.email,
+      issueType: request.body.issueType,
+      howToReproduce: request.body.howToReproduce,
+      expectedOutput: request.body.expectedOutput,
+      receivedOutput: request.body.receivedOutput,
+      userAgent: request.body.userAgent,
+      userDate: request.body.userDate,
+      source: request.query.source || 'direct'
     })
 
-})
+    // se for critico
+    if (request.body.issueType === 'CRITICAL') {
+        // using Twilio SendGrid's v3 Node.js Library
+        // https://github.com/sendgrid/sendgrid-nodejs
+        sgMail.setApiKey(sendGridKey)
+        const msg = {
+            to: 'sandro.herbst@gmail.com',
+            from: 'sandro@programaleiloes.com',
+            subject: 'BUG Critico reportado',
+            text: `
+                O usuário ${request.body.name} reportou um problema.
+            `,
+            html: `O usuário ${request.body.name} reportou um problema.`,
+        }
+        await sgMail.send(msg)
+    }
+
+    response.send("bug reportado com sucesso");
+
+  } catch (err) {
+    response.send('Erro ao enviar o formulário.')
+    console.log(err)    
+  }
+});
 
 app.get('/soma', (request, response) => {
     const a = parseInt(request.query.a)
--- testar-planilha.js
+++ testar-planilha.js
@@ -1,8 +1,21 @@
 const GoogleSpreadsheet = require('google-spreadsheet')
 const credentials = require('./bugtracker.json')
+const {promisify} = require('util')
 
+const addRowToSheet = async() => {
+    const doc = new GoogleSpreadsheet('1c3pwSVjWcoIbXq2thIvvDSVms0HD5v-HseZ90WKf44A')
+    await promisify(doc.useServiceAccountAuth)(credentials)
+    console.log('planilha aberta')
+    const info = await promisify(doc.getInfo)()
+    const worksheet = info.worksheets[0]
+    await promisify(worksheet.addRow)({name: 'Sandro', email: 'teste'})
+}
+
+addRowToSheet()
+
+
+/*
 const doc = new GoogleSpreadsheet('1c3pwSVjWcoIbXq2thIvvDSVms0HD5v-HseZ90WKf44A')
-
 doc.useServiceAccountAuth(credentials, (err) => {
     if(err) {
         console.log('não foi possível abrir a planilha')
@@ -17,3 +30,4 @@ doc.useServiceAccountAuth(credentials, (err) => {
         })
     }
 })
+*/
